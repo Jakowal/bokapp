07b728e28502586b10880de2ebc66f7a68199108
Jakob <jakob.walmann@outlook.com>
2023-11-16 21:33:16 +0100

    Updated fields

--- src/models/BookModel.ts
+++ src/models/BookModel.ts
@@ -5,10 +5,8 @@ export interface BookModel   {
   title?: string;
   price?: string;
   condition?: string;
-  totalprice?: number,
   sold?: string;
   category?: string;
-  subtitle?: string;
   soldFor?: string;
   soldTo?: string;
   catalogueNumber?: number;
@@ -17,100 +15,65 @@ export interface BookModel   {
   bookNumber?: number,
   catalogueEntry?: string;
   newCatalogueEntry?: string;
-  string?: string;
-  comma?: string;
-  colon?: string;
-  authorEntry?: string;
   publishedPlace?: string;
   publishedYear?: string;
-  totalNumberPublished?: number,
   subCategory?: string;
   firstLetter?: string;
   fulAuthorName?: string;
   catalogueEntrySubNumber?: string;
   comment?: string;
   boughtFor?: string;
-  amountBoughtFor?: number,
   edition?: string;
   numberOfBooks?: number,
-  gainOrLoss?: string;
-  gainOrLossTotal?: number,
   purchaseDate?: Date;
   registeredDate?: Date;
-  alternateName?: string;
   placeReference?: string;
   format?: string;
   binding?: string;
-  pageCount?: string;
   soldStatisticsName?: string;
-  totalPageCount?: string;
-  firstEditionTest?: boolean;
-  totalFirstEditionCount?: number,
   categoryShortName?: string;
-  categoryNumber?: string;
   catalogueEntryShort?: string;
-  soldText?: string;
-  nameSorting?: string;
   store?: string;
   firstLetterCategory?: string;
-  catalogueEntryShortSubPlace?: string
-  todaysDate?: string
+  lastChanged?: string
+  userId?: string
 }
 export const BookModelFieldTranslationsFromEnglish: {[key in keyof BookModel]: string } = {
   id: "id",
-  bookNumber: 'boknummer',
   authorLastName: 'Forfatter etternavn',
   authorFirstName: 'Forfatter fornavn',
   title: 'Tittel',
-  price: 'Pris',
+  price: 'Antatt verdi',
   condition: 'Tilstand',
-  totalprice: 'Totalpris',
-  sold: 'Solgt',
   category: 'Kategori',
-  subtitle: 'Undertittel',
-  soldFor: 'Solgt for',
-  soldTo: 'Solgt til',
+  catalogueBookNumber: 'Registreringsnummer',
+  publishedPlace: 'Utgivelsessted',
+  publishedYear: 'Utgivelsesår',
+  comment: 'Kommentar',
+  boughtFor: 'Kjøpt for',
+  placeReference: 'Plassreferanse',
+  binding: 'Innbinding',
+  format: 'Format',
+  store: 'Antikvariat',
   catalogueNumber: 'Katalog nr.',
-  catalogueBookNumber: 'katalogboknummer',
   saleDate: 'Salgsdato',
   catalogueEntry: 'kataloginnskrift',
   newCatalogueEntry: 'Ny kataloginnskrift',
-  string: 'Streng',
-  comma: 'Komma',
-  colon: 'Kolon',
-  authorEntry: 'Forfatterinnskrift',
-  publishedPlace: 'Utgivelsessted',
-  publishedYear: 'Utgivelsesår',
-  totalNumberPublished: 'Antall eks.',
   subCategory: 'Underkategori',
   firstLetter: 'Forbokstav',
   fulAuthorName: 'Fult forfatternavn',
   catalogueEntrySubNumber: 'Kataloginnskrift u nr',
-  comment: 'Kommentar',
-  boughtFor: 'Kjøpt for',
-  amountBoughtFor: 'Sum kjøpt for',
   edition: 'Utgave',
   numberOfBooks: 'Antall verk',
-  gainOrLoss: 'Gevinst eller tap',
-  gainOrLossTotal: 'Sum gevinst eller tap',
   purchaseDate: 'Kjøpsdato',
   registeredDate: 'Registreringsdato',
-  alternateName: 'Alternativt navn',
-  placeReference: 'Plassreferanse',
-  format: 'Format',
-  binding: 'Innbinding',
-  pageCount: 'Sideantall',
   soldStatisticsName: 'Solgtstatistikknavn',
-  totalPageCount: 'Sum sideantall',
-  firstEditionTest: 'Førsteutgavetest',
-  totalFirstEditionCount: 'Sum av førsteutgaver',
   categoryShortName: 'Kortnavn kategori',
-  categoryNumber: 'kategorisorteringsnummer',
   catalogueEntryShort: 'kataloginnskrift kort',
-  soldText: 'Solgttekst',
-  nameSorting: 'Navnesortering',
-  store: 'Antikvariat',
   firstLetterCategory: 'Forbokstav kategori',
-  catalogueEntryShortSubPlace: 'kataloginnskrift kort u plass',
-  todaysDate: 'd dato',
+  bookNumber: 'boknummer',
+  sold: 'Solgt',
+  soldFor: 'Solgt for',
+  soldTo: 'Solgt til',
+  lastChanged: 'Sist Endret',
 }
